b9dd365c14f23970eed4631aa1020cca07fcc5d6
Michael Benvenuti <mike789@gmail.com>
2012-11-27 02:22:38 -0500

    install script working

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Install dotfiles
+
+#Excludes
+
+SCRIPTDIR=$(dirname $0)
+INSTALLDIR=$HOME
+BACKUP=.dotfiles.old
+
+for FILE in $SCRIPTDIR/.*;
+do
+   if [ ! -d $FILE ]
+   then
+      FILENAME=$(basename $FILE);
+      if [ -f $INSTALLDIR/$FILENAME ];
+      then
+         if [ -h $INSTALLDIR/$FILENAME ] && [ $(readlink $INSTALLDIR/$FILENAME) == $SCRIPTDIR/$FILENAME ]
+         then
+            continue
+         fi
+         if [ ! -d $INSTALLDIR/$BACKUP ];
+         then 
+            echo "mkdir -p $INSTALLDIR/$BACKUP"
+            mkdir -p $INSTALLDIR/$BACKUP
+         fi
+         echo "mv $INSTALLDIR/$FILENAME $INSTALLDIR/$BACKUP/$FILENAME"
+         mv $INSTALLDIR/$FILENAME $INSTALLDIR/$BACKUP/$FILENAME
+      fi
+      echo "ln -st $INSTALLDIR $SCRIPTDIR/$FILENAME"
+      ln -st $INSTALLDIR $SCRIPTDIR/$FILENAME
+   fi
+done
+
+source $HOME/.bashrc
+exec bash
